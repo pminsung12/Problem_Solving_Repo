555a5f2b0a77906491e3391c8a0cd9b628c64b1b
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2021-12-02 22:40:45 +0900

    1st

---
+++ c_prac/1546.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    int N;//과목 개수
+    int M = 0;
+    double avg = 0;
+    int score[1000] = {0,};
+    scanf("%d\n", &N);
+
+    //최댓값 알아내기
+    for (int i = 0; i<N;i++){
+        scanf("%d\n", &score[i]);
+        if (score[i] >= M){
+            M = score[i];
+        }
+        //score초기화해야하나
+    }
+
+    //평균구하기
+    for(int j = 0;j<N;j++){
+        avg += score[i];
+    } 
+
+    //새로운 평균
+    printf("%.4lf", avg/M*100);
+    
+
+    return 0;
+}
