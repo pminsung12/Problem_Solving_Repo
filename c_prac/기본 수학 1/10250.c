2f993816314edb03d27a0d9f230ad28b9de69ed0
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2021-12-14 23:20:41 +0900

    211214

--- c_prac/기본 수학 1/10250.c
+++ c_prac/기본 수학 1/10250.c
@@ -5,8 +5,14 @@ int main(){
     scanf("%d\n", &T);
     for(int i=0;i<T;i++){
         scanf("%d %d %d",&H,&W,&N);
-        if(N/H+1<10) printf("%d0%d\n",N%H,N/H+1);
-        else printf("%d%d\n",N%H,N/H+1);
+        int a = N%H;
+        int b = N/H+1;//1~6 1 7~12 2 13~18 3
+        if(N%H==0) {
+            a = H;
+            b = N/H;
+        }
+        if(b<10) printf("%d0%d\n",a,b);
+        else printf("%d%d\n",a,b);
     }
     return 0;
 }
