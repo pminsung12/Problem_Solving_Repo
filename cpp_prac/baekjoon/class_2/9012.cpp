5d4a860f277bee7b63605ca3c84f20e1f7d7220b
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-02-07 00:03:54 +0900

    220207

---
+++ cpp_prac/baekjoon/class_2/9012.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+
+    char arr[51];
+    while(T--){
+        cin>>arr;
+        int len=strlen(arr);
+        for(int i=0;i<n;i++){
+            if(arr[i]=='('){
+                if(arr[n-1-i]==')') no
+            }
+        }
+        cout<<"NO"<<'\n';
+        
+    }
+    return 0;
+}
+
+(()())((()))
+
