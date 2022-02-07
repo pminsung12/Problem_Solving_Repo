2d86345f9a2b46bb505d4cc53c83949cace503fc
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-02-07 14:21:41 +0900

    220207

--- cpp_prac/baekjoon/class_2/9012.cpp
+++ cpp_prac/baekjoon/class_2/9012.cpp
@@ -1,25 +1,31 @@
+//스택문제 괄호는 스택이다!
 #include <iostream>
-#include <cstring>
+#include <stack>
+#include <string>
 using namespace std;
+
+bool IsValid(string str){
+    stack<char> stk;
+    for(int i=0;i<str.length();i++){
+        if(str[i]=='(') stk.push(str[i]);
+        else {
+            if(stk.empty()) return false;
+            stk.pop();
+        }
+    }
+    if(stk.empty()) return true;
+    else return false;
+}
 
 int main(){
     int T;
     cin>>T;
-
-    char arr[51];
+    
+    string str;
     while(T--){
-        cin>>arr;
-        int len=strlen(arr);
-        for(int i=0;i<n;i++){
-            if(arr[i]=='('){
-                if(arr[n-1-i]==')') no
-            }
-        }
-        cout<<"NO"<<'\n';
-        
+        cin>>str;
+        if(IsValid(str)) cout<<"YES"<<'\n';
+        else cout<<"NO"<<'\n';
     }
     return 0;
 }
-
-(()())((()))
-
