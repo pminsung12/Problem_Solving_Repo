32641b2f00e238d83a978af873afbf4ecb2811ce
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-03-11 20:34:19 +0900

    solved

--- cpp_prac/baekjoon/BFS/토마토.cpp
+++ cpp_prac/baekjoon/BFS/토마토.cpp
@@ -9,35 +9,27 @@ int N,M;
 int dx[4]={0,0,-1,1};
 int dy[4]={1,-1,0,0};
 int tmt[1001][1001];
-bool visited[1001][1001];
+// bool visited[1001][1001];
+// int level[1001][1001];
 queue<pair<int,int>> q;
-int cnt=0;
 
-int bfs(){
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            if(tmt[i][j]&&!visited[i][j]){
-                visited[i][j]=true;
-                q.push(make_pair(i,j));
-                while(!q.empty()){
-                    int x=q.front().first;
-                    int y=q.front().second;
-                    q.pop();
-                    for(int k=0;k<4;k++){
-                        int new_x=x+dx[k];
-                        int new_y=y+dy[k];
-                        if(new_x>=0&&new_x<N&&new_y>=0&&new_y<M&&
-                            tmt[new_x][new_y]==0&&!visited[new_x][new_y]){
-                            visited[new_x][new_y]=true;
-                            q.push(make_pair(new_x,new_y));
-
-                        }
-                    }
-                    cnt++;
-                }
+void bfs(){
+    while(!q.empty()){
+        int x=q.front().first;
+        int y=q.front().second;
+        q.pop();
+        for(int k=0;k<4;k++){
+            int new_x=x+dx[k];
+            int new_y=y+dy[k];
+            
+            if(new_x>=0&&new_x<N&&new_y>=0&&new_y<M&&
+                tmt[new_x][new_y]==0){
+                tmt[new_x][new_y]=tmt[x][y]+1;
+                q.push(make_pair(new_x,new_y));
             }
         }
     }
+    return;
 }
 
 int main(){
@@ -48,9 +40,28 @@ int main(){
     for(int i=0;i<N;i++){
         for(int j=0;j<M;j++){
             cin>>tmt[i][j];
+            if(tmt[i][j]==1) q.push(make_pair(i,j));
         }
     }
+    bfs();
 
-                   
+    // for(int i=0;i<N;i++){
+    //     for(int j=0;j<M;j++){
+    //         cout<<tmt[i][j]<<' ';
+    //     }
+    //     cout<<'\n';
+    // }
+    int max=0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(tmt[i][j]==0){
+                cout<<"-1"<<'\n';
+                return 0;
+            }
+            if(max<tmt[i][j]) max=tmt[i][j];
+        }
+    }
+    
+    cout<<max-1<<'\n';            
     return 0;
 }
