5fe0e1aabed342abb9c00c478fae1e9e2fd12667
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-04-13 20:21:34 +0900

    solving

---
+++ cpp_prac/baekjoon/DFS/알파벳.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define MAX 21
+
+int R,C;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>R>>C;
+    for(int i=0;i<R;i++){
+        
+    }    
+    return 0;
+}
