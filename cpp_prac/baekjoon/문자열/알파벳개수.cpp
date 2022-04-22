bb8f29073c1c0503b6e75c3288d48ad29a5d0f77
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-04-23 01:12:04 +0900

    solved

---
+++ cpp_prac/baekjoon/문자열/알파벳개수.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int alpha[27]={0,};
+    string s;
+    cin>>s;
+
+    for(int i=0;i<s.size();i++){
+        alpha[s[i]-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        cout<<alpha[i]<<' ';
+    }
+    return 0;
+}
