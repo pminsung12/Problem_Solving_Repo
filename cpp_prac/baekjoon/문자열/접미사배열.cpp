e041d2c01177a3aac074bbd8edd553e50a8fbd45
pminsung12 <52368015+pminsung12@users.noreply.github.com>
2022-04-25 00:54:03 +0900

    solved

---
+++ cpp_prac/baekjoon/문자열/접미사배열.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string s;
+    string tmp;
+    vector<string> v;
+
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        
+        tmp=s.substr(i,s.size()-i);
+        v.push_back(tmp);
+    }
+    sort(v.begin(),v.end());
+    for(auto i:v){
+        cout<<i<<'\n';
+    }
+    return 0;
+}
