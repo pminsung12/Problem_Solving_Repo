4f8182b48c342d2448bada1e3a163277385aceec
Minseong <pminsung12@naver.com>
2022-05-02 00:23:24 +0900

    saved

--- cpp_prac/baekjoon/문자열/접미사배열.cpp
+++ cpp_prac/baekjoon/문자열/접미사배열.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -23,30 +22,4 @@ int main(){
         cout<<i<<'\n';
     }
     return 0;
-=======
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    string s;
-    string tmp;
-    vector<string> v;
-
-    cin>>s;
-    for(int i=0;i<s.size();i++){
-        
-        tmp=s.substr(i,s.size()-i);
-        v.push_back(tmp);
-    }
-    sort(v.begin(),v.end());
-    for(auto i:v){
-        cout<<i<<'\n';
-    }
-    return 0;
->>>>>>> solved
 }
