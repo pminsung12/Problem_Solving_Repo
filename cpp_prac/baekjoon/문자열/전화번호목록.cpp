5ebf378cf6c1b66c15a4d01609b47160ca4f3002
Minseong <pminsung12@naver.com>
2022-05-01 23:55:48 +0900

    added another solution

--- cpp_prac/baekjoon/문자열/전화번호목록.cpp
+++ cpp_prac/baekjoon/문자열/전화번호목록.cpp
@@ -1,3 +1,4 @@
+//풀이1
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -40,3 +41,47 @@ int main(){
     }
     return 0;
 }
+//풀이 2 substr()
+/*
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int T;
+    cin >> T;
+    
+    string num[10001];
+    int n;
+    
+    while(T--) {
+        bool isValid = true;
+        cin >> n;
+        
+        for (int i = 0; i < n; i++) {
+            cin >> num[i];
+        }
+ 
+        sort(num, num+n);
+ 
+        for (int j = 0; j < n-1; j++) {
+            int current = num[j].length();
+            int next = num[j+1].length();
+ 
+            if (current >= next) continue;
+            if (num[j + 1].substr(0, current)== num[j]) {
+                isValid = false;
+                cout << "NO" << '\n';
+                break;
+            }
+        }
+ 
+        if (isValid) cout << "YES" << '\n';
+    }
+    return 0;
+}
+*/
