0dba31883ca27eae150a1be27f4efd7a9e8b3576
Minseong <pminsung12@naver.com>
2022-05-03 01:31:04 +0900

    unsolved yet

---
+++ cpp_prac/baekjoon/문자열/찾기(unfinished).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int gettingK(string t, string p, int n, int m){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(t[i+j-1]!=p[j]){
+                
+            }
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string t;
+    string p;
+    cin>>t>>p;
+
+    int n=t.length();
+    int m=p.length();
+    
+    return 0;
+}
